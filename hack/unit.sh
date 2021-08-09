15776c1d1d836257f4fa087aeadbfe9259e171e4
wcecs <82305632+wcecs@users.noreply.github.com>
2021-08-09 10:04:16 +0530

    Update unit.sh

--- hack/unit.sh
+++ hack/unit.sh
@@ -9,7 +9,7 @@ TEST_FLAGS="${TEST_FLAGS:--v}"
 
 if [ "${IS_CONTAINER}" != "false" ]; then
   eval "$(go env)"
-  cd "${GOPATH}"/src/github.com/metal3-io/baremetal-operator
+  cd "${GOPATH}"/src/github.com/wcecs/baremetal-operator
   export XDG_CACHE_HOME="/tmp/.cache"
 
   export COVER_PROFILE="${ARTIFACTS}"/cover.out
@@ -24,9 +24,9 @@ else
     --env DEPLOY_RAMDISK_URL=http://172.22.0.1/images/ironic-python-agent.initramfs \
     --env IRONIC_ENDPOINT=http://localhost:6385/v1/ \
     --env IRONIC_INSPECTOR_ENDPOINT=http://localhost:5050/v1/ \
-    --volume "${PWD}:/go/src/github.com/metal3-io/baremetal-operator:rw,z" \
+    --volume "${PWD}:/go/src/github.com/wcecs/baremetal-operator:rw,z" \
     --entrypoint sh \
-    --workdir /go/src/github.com/metal3-io/baremetal-operator \
+    --workdir /go/src/github.com/wcecs/baremetal-operator \
     registry.hub.docker.com/library/golang:1.16 \
-    /go/src/github.com/metal3-io/baremetal-operator/hack/unit.sh "${@}"
+    /go/src/github.com/wcecs/baremetal-operator/hack/unit.sh "${@}"
 fi;
