c7a2e8b1613b4d956da9c3132fdb38f957031159
wcecs <82305632+wcecs@users.noreply.github.com>
2021-08-09 10:13:35 +0530

    Update configdrive_test.go

--- pkg/provisioner/ironic/configdrive_test.go
+++ pkg/provisioner/ironic/configdrive_test.go
@@ -7,12 +7,12 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/baremetalintrospection/v1/introspection"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
-	"github.com/metal3-io/baremetal-operator/pkg/bmc"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/fixture"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/clients"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/testserver"
+	"github.com/wcecs/baremetal-operator/apis/metal3.io/v1alpha1"
+	"github.com/wcecs/baremetal-operator/pkg/bmc"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner/fixture"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/clients"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/testserver"
 )
 
 func TestEmpty(t *testing.T) {
