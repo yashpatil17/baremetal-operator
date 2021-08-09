9db3336f240253e7b1e5425f949259941bed1771
wcecs <82305632+wcecs@users.noreply.github.com>
2021-08-09 10:24:45 +0530

    Update provisioner.go

--- pkg/provisioner/provisioner.go
+++ pkg/provisioner/provisioner.go
@@ -6,9 +6,9 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	metal3v1alpha1 "github.com/metal3-io/baremetal-operator/apis/metal3.io/v1alpha1"
-	"github.com/metal3-io/baremetal-operator/pkg/bmc"
-	"github.com/metal3-io/baremetal-operator/pkg/hardware"
+	metal3v1alpha1 "github.com/wcecs/baremetal-operator/apis/metal3.io/v1alpha1"
+	"github.com/wcecs/baremetal-operator/pkg/bmc"
+	"github.com/wcecs/baremetal-operator/pkg/hardware"
 )
 
 /*
