a12e8ce45b699f4b70140b9a0a2aaa70dbe34d5d
wcecs <82305632+wcecs@users.noreply.github.com>
2021-08-09 10:07:50 +0530

    Update generate.sh

--- hack/generate.sh
+++ hack/generate.sh
@@ -29,7 +29,7 @@ else
     --env DEPLOY_RAMDISK_URL=http://172.22.0.1/images/ironic-python-agent.initramfs \
     --env IRONIC_ENDPOINT=http://localhost:6385/v1/ \
     --env IRONIC_INSPECTOR_ENDPOINT=http://localhost:5050/v1/ \
-    --volume "${PWD}:/go/src/github.com/metal3-io/baremetal-operator:rw,z" \
+    --volume "${PWD}:/go/src/github.com/wcecs/baremetal-operator:rw,z" \
     --entrypoint sh \
     --workdir /go/src/github.com/wcecs/baremetal-operator \
     registry.hub.docker.com/library/golang:1.16 \
