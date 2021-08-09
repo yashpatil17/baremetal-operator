dae79f3db3413cee55862c352a16ab3aacafc3cc
wcecs <82305632+wcecs@users.noreply.github.com>
2021-08-09 10:18:59 +0530

    Update findhost_test.go

--- pkg/provisioner/ironic/findhost_test.go
+++ pkg/provisioner/ironic/findhost_test.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/gophercloud/gophercloud/openstack/baremetal/v1/nodes"
 
-	"github.com/metal3-io/baremetal-operator/pkg/bmc"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/clients"
-	"github.com/metal3-io/baremetal-operator/pkg/provisioner/ironic/testserver"
+	"github.com/wcecs/baremetal-operator/pkg/bmc"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/clients"
+	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/testserver"
 )
 
 func TestFindExistingHost(t *testing.T) {
