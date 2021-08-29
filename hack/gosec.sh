486fd0df0b846977eda51df39f59c07dd407219d
Shweta Chavan <41147364+shweta50@users.noreply.github.com>
2021-08-29 19:14:14 +0530

    Update gosec.sh

--- hack/gosec.sh
+++ hack/gosec.sh
@@ -12,9 +12,9 @@ if [ "${IS_CONTAINER}" != "false" ]; then
 else
   "${CONTAINER_RUNTIME}" run --rm \
     --env IS_CONTAINER=TRUE \
-    --volume "${PWD}:/go/src/github.com/wcecs/baremetal-operator:ro,z" \
+    --volume "${PWD}:/go/src/github.com/shweta50/baremetal-operator:ro,z" \
     --entrypoint sh \
-    --workdir /go/src/github.com/wcecs/baremetal-operator \
+    --workdir /go/src/github.com/shweta50/baremetal-operator \
     registry.hub.docker.com/securego/gosec:latest \
-    /go/src/github.com/wcecs/baremetal-operator/hack/gosec.sh "${@}"
+    /go/src/github.com/shweta50/baremetal-operator/hack/gosec.sh "${@}"
 fi;
