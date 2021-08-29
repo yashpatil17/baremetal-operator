ef3b8dd3b2e75f8117ce1488f4815fb101327f07
Shweta Chavan <41147364+shweta50@users.noreply.github.com>
2021-08-29 19:04:30 +0530

    Update power_test.go

--- pkg/provisioner/ironic/power_test.go
+++ pkg/provisioner/ironic/power_test.go
@@ -9,10 +9,10 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/baremetalintrospection/v1/introspection"
 	"github.com/stretchr/testify/assert"
 
-	metal3v1alpha1 "github.com/wcecs/baremetal-operator/apis/metal3.io/v1alpha1"
-	"github.com/wcecs/baremetal-operator/pkg/bmc"
-	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/clients"
-	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/testserver"
+	metal3v1alpha1 "github.com/shweta50/baremetal-operator/apis/metal3.io/v1alpha1"
+	"github.com/shweta50/baremetal-operator/pkg/bmc"
+	"github.com/shweta50/baremetal-operator/pkg/provisioner/ironic/clients"
+	"github.com/shweta50/baremetal-operator/pkg/provisioner/ironic/testserver"
 )
 
 func TestPowerOn(t *testing.T) {
