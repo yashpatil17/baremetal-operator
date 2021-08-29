1c16cefb24e497b240cdf973c3b99f1826bb68ad
Shweta Chavan <41147364+shweta50@users.noreply.github.com>
2021-08-29 18:50:36 +0530

    Update irmc.go

--- pkg/bmc/irmc.go
+++ pkg/bmc/irmc.go
@@ -3,7 +3,7 @@ package bmc
 import (
 	"net/url"
 
-	metal3v1alpha1 "github.com/wcecs/baremetal-operator/apis/metal3.io/v1alpha1"
+	metal3v1alpha1 "github.com/shweta50/baremetal-operator/apis/metal3.io/v1alpha1"
 )
 
 func init() {
