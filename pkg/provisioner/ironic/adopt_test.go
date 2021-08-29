dc2a9ad7e2514ba0d6df896c44b6094b1349c15b
Shweta Chavan <41147364+shweta50@users.noreply.github.com>
2021-08-29 19:09:24 +0530

    Update adopt_test.go

--- pkg/provisioner/ironic/adopt_test.go
+++ pkg/provisioner/ironic/adopt_test.go
@@ -8,10 +8,10 @@ import (
 	"github.com/gophercloud/gophercloud/openstack/baremetalintrospection/v1/introspection"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/wcecs/baremetal-operator/pkg/bmc"
-	"github.com/wcecs/baremetal-operator/pkg/provisioner"
-	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/clients"
-	"github.com/wcecs/baremetal-operator/pkg/provisioner/ironic/testserver"
+	"github.com/shweta50/baremetal-operator/pkg/bmc"
+	"github.com/shweta50/baremetal-operator/pkg/provisioner"
+	"github.com/shweta50/baremetal-operator/pkg/provisioner/ironic/clients"
+	"github.com/shweta50/baremetal-operator/pkg/provisioner/ironic/testserver"
 )
 
 func TestAdopt(t *testing.T) {
