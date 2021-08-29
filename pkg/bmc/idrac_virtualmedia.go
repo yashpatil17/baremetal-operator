40d7bb2432269b6927a84dc2f5ca36fcb64b5887
Shweta Chavan <41147364+shweta50@users.noreply.github.com>
2021-08-29 18:52:37 +0530

    Update idrac_virtualmedia.go

--- pkg/bmc/idrac_virtualmedia.go
+++ pkg/bmc/idrac_virtualmedia.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/url"
 
-	metal3v1alpha1 "github.com/wcecs/baremetal-operator/apis/metal3.io/v1alpha1"
+	metal3v1alpha1 "github.com/shweta50/baremetal-operator/apis/metal3.io/v1alpha1"
 )
 
 func init() {
